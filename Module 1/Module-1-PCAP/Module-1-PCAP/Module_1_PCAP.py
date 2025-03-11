93f16605df30312c14d576d890dc3119a8951c1e
Chase-Bennett <bennettchase638@gmail.com>
2025-03-11 11:18:00 -0400

    1.1 Done

--- Module 1/Module-1-PCAP/Module-1-PCAP/Module_1_PCAP.py
+++ Module 1/Module-1-PCAP/Module-1-PCAP/Module_1_PCAP.py
@@ -1,3 +1,59 @@
 
-print("test")
-print("vsc Test")
+
+
+
+
+
+
+
+from math import sin, pi
+
+print(sin(pi / 2))
+
+pi = 3.14
+
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+
+print(sin(pi / 2))
+
+
+
+
+
+
+
+
+pi = 3.14
+
+def sin(x):
+    if 2 * x == pi:
+        return 0.99999999
+    else:
+        return None
+
+print(sin(pi / 2))
+
+from math import sin, pi
+
+print(sin(pi / 2))
+
+import math
+  
+#for name in dir(math):
+   # print(name, end="\t")
+
+print(math.factorial(10))
+
+
+
+from math import e, exp, log
+
+print(pow(e, 1) == exp(log(e)))
+print(pow(2, 2) == exp(2 * log(2)))
+print(log(e, e) == exp(0))
