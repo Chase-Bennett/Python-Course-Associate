6f69348962d011d75a6da259e4da229a39287fdc
Chase-Bennett <bennettchase638@gmail.com>
2025-03-21 10:13:14 -0400

    Lab start

--- Module 2/Module-2.py
+++ Module 2/Module-2.py
@@ -77,3 +77,42 @@ print(' Alpha'.capitalize())
 print('123'.capitalize())
 print( "abgd" . capitalize  ())
 print("αΒΓδ".capitalize())
+
+def mysplit(strng):
+# return [] if string is empty or contains whitespaces only
+    
+    # prepare a list to return
+
+    # prepare a word to build subsequent words
+
+    # check if we are currently inside a word (i.e., if the string starts with a word)
+
+    # iterate through all the characters in the string
+
+        # if we are currently inside a string...
+
+            # ... and the current character is not a space...
+
+                # ... update the current word
+
+                # ... otherwise, we've reached the end of the word so we need to append it to the list...
+
+                # ... and signal the fact that we are outside the word now
+
+            # if we are outside the word and we've reached a non-white character...
+
+                # ... it means that a new word has begun so we need to remember it and...
+
+                # ... store the first letter of the new word
+
+    # if we've left the string and there is a non-empty string in the word, we need to update the list
+
+    # return the list to the invoker
+
+
+print(mysplit("To be or not to be, that is the question"))
+print(mysplit("To be or not to be,that is the question"))
+print(mysplit("   "))
+print(mysplit(" abc "))
+print(mysplit(""))
+print("Hello world")
