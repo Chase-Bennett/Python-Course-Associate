18377e41213785ae1846f273bdd6fec9c2cd74de
Chase-Bennett <bennettchase638@gmail.com>
2025-03-27 11:06:28 -0400

    3.2 Labs Done

--- Module 3/Module-3.py
+++ Module 3/Module-3.py
@@ -115,7 +115,7 @@ another_stack.push(little_stack.pop() + 1)
 funny_stack.push(another_stack.pop() - 2)
 
 print(funny_stack.pop())
-'''
+
 
 class Stack:
     def __init__(self):
@@ -156,3 +156,74 @@ print(stack_object.get_sum())
 
 for i in range(5):
     print(stack_object.pop())
+    
+    
+    
+
+
+
+class Stack:
+    def __init__(self):
+        self.__stk = []
+
+    def push(self, val):
+        self.__stk.append(val)
+
+    def pop(self):
+        val = self.__stk[-1]
+        del self.__stk[-1]
+        return val
+
+
+class CountingStack(Stack):
+    def __init__(self):
+        Stack.__init__(self)
+        self.__counter = 0
+
+    def get_counter(self):
+        return self.__counter
+
+    def pop(self):
+        self.__counter += 1
+        return Stack.pop(self)
+
+
+stk = CountingStack()
+for i in range(100):
+    stk.push(i)
+    stk.pop()
+print(stk.get_counter())
+
+'''
+
+
+class QueueError(IndexError):
+    pass
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def put(self, elem):
+        self.queue.insert(0, elem)
+
+    def get(self):
+        if len(self.queue) > 0:
+            elem = self.queue[-1]
+            del self.queue[-1]
+            return elem
+        else:
+            raise QueueError
+
+
+que = Queue()
+que.put(1)
+que.put("dog")
+que.put(False)
+try:
+    for i in range(4):
+        print(que.get())
+except:
+    print("Queue error")
+    
