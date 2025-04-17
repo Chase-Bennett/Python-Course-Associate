c206bbce14a92ab36da01d54f4e7d652c475ca59
Chase-Bennett <bennettchase638@gmail.com>
2025-04-17 11:10:18 -0400

    3.5 Done

--- Module 3/Module-3.py
+++ Module 3/Module-3.py
@@ -452,7 +452,7 @@ if __name__ == "__main__":
     point2 = Point(1, -200)
     print(point1.distance_from_point(point2))  # → 1.4142135623730951
     print(point2.distance_from_xy(2, 90))       # → 1.4142135623730951
-    '''
+    
 
 
 
@@ -508,9 +508,154 @@ class Triangle:
         #
 
 
-triangle = Triangle(Point(0, 0), Point(1, 0), Point(454450, 1))
+triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
+print(triangle.perimeter())
+    
+import math
+
+
+class Point:
+    def __init__(self, x=0.0, y=0.0):
+        self.__x = x
+        self.__y = y
+
+    def getx(self):
+        return self.__x
+
+    def gety(self):
+        return self.__y
+
+    def distance_from_xy(self, x, y):
+        return math.hypot(abs(self.__x - x), abs(self.__y - y))
+
+    def distance_from_point(self, point):
+        return self.distance_from_xy(point.getx(), point.gety())
+
+
+class Triangle:
+    def __init__(self, vertice1, vertice2, vertice3):
+        self.__vertices = [vertice1, vertice2, vertice3]
+
+    def perimeter(self):
+        per = 0
+        for i in range(3):
+            per += self.__vertices[i].distance_from_point(self.__vertices[(i + 1) % 3])
+        return per
+
+
+triangle = Triangle(Point(0, 0), Point(1, 0), Point(0, 1))
 print(triangle.perimeter())
     
 
-point1 = Point(0, 0)
-point2 = Point(1, 2)
+class Star:
+    def __init__(self, name, galaxy):
+        self.name = name
+        self.galaxy = galaxy
+
+
+sun = Star("Sun", "Milky Way")
+print(sun)
+
+
+
+class Star:
+    def __init__(self, name, galaxy):
+        self.name = name
+        self.galaxy = galaxy
+
+    def __str__(self):
+        return self.name + ' in ' + self.galaxy
+
+
+sun = Star("Sun", "Milky Way")
+print(sun)
+
+
+class Vehicle:
+    pass
+
+
+class LandVehicle(Vehicle):
+    pass
+
+
+class TrackedVehicle(LandVehicle):
+    pass
+
+
+my_vehicle = Vehicle()
+my_land_vehicle = LandVehicle()
+my_tracked_vehicle = TrackedVehicle()
+
+for obj in [my_vehicle, my_land_vehicle, my_tracked_vehicle]:
+    for cls in [Vehicle, LandVehicle, TrackedVehicle]:
+        print(isinstance(obj, cls), end="\t")
+    print()
+    
+ 
+# Testing properties: class variables.
+class Super:
+    supVar = 1
+
+
+class Sub(Super):
+    subVar = 2
+
+
+obj = Sub()
+
+print(obj.subVar)
+print(obj.supVar)
+   
+
+# Testing properties: instance variables.
+class Super:
+    def __init__(self):
+        self.supVar = 11
+
+
+class Sub(Super):
+    def __init__(self):
+        super().__init__()
+        self.subVar = 12
+
+
+obj = Sub()
+
+print(obj.subVar)
+print(obj.supVar)
+    '''
+class Level1:
+    variable_1 = 100
+    def __init__(self):
+        self.var_1 = 101
+
+    def fun_1(self):
+        return 102
+
+
+class Level2(Level1):
+    variable_2 = 200
+    def __init__(self):
+        super().__init__()
+        self.var_2 = 201
+    
+    def fun_2(self):
+        return 202
+
+
+class Level3(Level2):
+    variable_3 = 300
+    def __init__(self):
+        super().__init__()
+        self.var_3 = 301
+
+    def fun_3(self):
+        return 302
+
+
+obj = Level3()
+
+print(obj.variable_1, obj.var_1, obj.fun_1())
+print(obj.variable_2, obj.var_2, obj.fun_2())
+print(obj.variable_3, obj.var_3, obj.fun_3())
